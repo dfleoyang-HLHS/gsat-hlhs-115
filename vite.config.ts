4e9ec527f55d60324cd9eb86c2f56936de599084
dfleoyang-HLHS <dfleoyang@hlhs.hlc.edu.tw>
2026-02-25 15:52:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
+  // 關鍵：這裡必須填入您的 GitHub 儲存庫名稱（前後都要有斜線）
   base: '/gsat-hlhs-115/', 
+  
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
