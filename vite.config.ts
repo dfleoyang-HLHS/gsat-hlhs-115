841e39e5201fc1448317c1240d0e8e2e3512b13f
dfleoyang-HLHS <dfleoyang@hlhs.hlc.edu.tw>
2026-02-25 15:51:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/gsat-hlhs-115/', 
   plugins: [react(), tailwindcss()],
 })
